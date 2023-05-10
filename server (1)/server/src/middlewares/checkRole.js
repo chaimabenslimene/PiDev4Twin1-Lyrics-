b8fb0e38836d68a4034be26fe4d6df7444734147
chaima Ben Slimene <benslimene.chaima@esprit.tn>
2023-05-10 23:45:22 +0200

    final commit

--- server (1)/server/src/middlewares/checkRole.js
+++
@@ -1,9 +0,0 @@
-const isSuperAdmin = (req, res, next) => {
-  req.user?.isAdmin === true ? next() : res.sendStatus(403);
-};
-
-const isAdmin = (req, res, next) => {
-  req.user?.isAdmin === true ? next() : res.sendStatus(403);
-};
-
-module.exports = { isSuperAdmin, isAdmin };
